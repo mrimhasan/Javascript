c8f4f34caef7005f96eaf95e10288149d3111091
Imran Hasan <108717664+mrimhasan@users.noreply.github.com>
2023-07-05 12:18:26 +0530

    Add files via upload

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version
+//alert(3+3) //we are using Nodejs not browser
+
+console.log(3
+  +3           //code readability should be high
+)
+
+let age = 18
+let name = "imran"
+let isloggedIn = true
+
+/*data types*/
+
+//number => range 2 to the power of 53
+//bigint
+//string ""
+//boolean => true or false
+//null =>standalone value
+//undefined
+//symbol =>unique//generally used in react
+
+//object
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object // boht se lang. ki khaami bhi bolte hain 
